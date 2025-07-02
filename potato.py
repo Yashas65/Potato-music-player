40accf6727261bdea8319e830221789e5af74e50
Yashas <parteyashas@gmail.com>
2025-07-02 13:42:30 +0000

    homepage ready

---
+++ potato.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+
+from kivy.uix.screenmanager import ScreenManager , Screen
+
+    
+    
+class Home(Screen):
+    pass
+
+class Player(Screen):
+    pass
+
+class Add_Music(Screen):
+    pass
+
+class Potato(App):
+    def build(self):
+        pass
+Potato().run()
