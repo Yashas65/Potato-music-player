5ec85b26634885d22921ad2dad7264fce5515fe8
Yashas <parteyashas@gmail.com>
2025-07-02 14:15:55 +0000

    distributed code across files

---
+++ a_musc.py
@@ -0,0 +1,4 @@
+from kivy.uix.screenmanager import Screen
+
+class Add_Music(Screen):
+    pass
--- potato.py
+++ potato.py
@@ -1,19 +1,23 @@
 from kivy.app import App
 
 from kivy.uix.screenmanager import ScreenManager , Screen
+from a_musc import Add_Music
+from player import Player
+from kivy.lang import Builder
 
-    
+
+Builder.load_file('a_musc.kv')
+Builder.load_file('player.kv')
     
 class Home(Screen):
     pass
 
-class Player(Screen):
-    pass
 
-class Add_Music(Screen):
-    pass
 
 class Potato(App):
     def build(self):
+        s = ScreenManager()
+        s.add_widget(Player(name='player'))
+        s.add_widget(Add_Music(name='add_music'))
         pass
 Potato().run()
