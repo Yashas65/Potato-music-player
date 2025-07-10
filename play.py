50924298bbd5c5b10236749634f466aa67d65576
Yashas <parteyashas@gmail.com>
2025-07-10 15:00:27 +0000

    added music controls

--- play.py
+++ play.py
@@ -1,22 +1,43 @@
 from kivy.uix.screenmanager import Screen
-import pygame
+import time
+from pygame.mixer import music
+
+
+def updt_status(self , dis):
+    self.ids.sttus = f'{dis}'
+    time.sleep(0.3)
+    self.ids.sttus = ''
+    print(dis)
 
 class Play(Screen):
 
 
-
     def load(self):#load and play
-        pygame.mixer.music.load(self)
-        pygame.mixer.music.play()
+        music.load(self)
+        music.set_volume(1.0)
+        music.play()
 
     def play(self):
-        pygame.mixer.music.play()
+        try:
+            music.play()
+            updt_status(self ,'|>')
+        except Exception as e:
+            updt_status(self , e)
 
     def pause(self):
-        pygame.mixer.music.pause()
+        music.pause()
+        updt_status(self , '||')
 
-    def unpause(self):
-        pygame.mixer.music.unpause()
 
-    def stop(self):
-        pygame.mixer.music.stop()
+
+    def in_vol(self):
+        self.volume = min(1.0, self.volume+0.1)
+        music.setvolume(self.volume)
+        updt_status(self , '+')
+
+    def dec_vol(self):
+        self.volume = max(0.0, self.volume-0.1)
+        music.setvolume(self.volume)
+        updt_status(self , '-')
+
+     
